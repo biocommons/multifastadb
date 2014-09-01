b988c5509db9bd63e96423b6874c55bd30c31a1d
Reece Hart <reecehart@gmail.com>
2014-09-01 10:25:00 -0700

    setup.py: make version_handler return x.y.0 instead of x.y for tagged releases

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from ez_setup import use_setuptools
 use_setuptools()
 
+import re
+
 from setuptools import setup, find_packages
 
 
@@ -8,6 +10,12 @@ def version_handler(mgr, options):
     version = mgr.get_current_version()
     if version.endswith('dev'):
         version += '-' + mgr._invoke('log', '-l1', '-r.', '--template', '{node|short}').strip()
+    elif re.match('^\d+\.\d+$',version):
+        # StrictVersion considers x.y == x.y.0 and drops the .0 from a
+        # repo tag.  Add it back and ensure that it's really a tag for
+        # our parent.
+        version += '.0'
+        assert version in mgr.get_parent_tags('tip')
     return version
 
 setup(
